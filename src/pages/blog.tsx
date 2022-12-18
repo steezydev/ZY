bfc9fbb982a1b3509e2040f7e1b2e5bb4adcf0fa
Ivan Ilichev <swggnuko.dev@gmail.com>
2022-12-18 22:51:29 +0300

    feat: :sparkles: null if no blogs are available

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -24,32 +24,34 @@ export default function BlogPage({ blogs }: BlogPageProps) {
         <section className='backdrop-blur-sm md:backdrop-blur-none'>
           <div className='layout min-h-screen py-28'>
             <Head slogan='CHËCK EM ÖUT'>Blog</Head>
-            <div className='mt-20 flex flex-col md:grid md:grid-cols-2 md:gap-10'>
-              {blogs.map((item: BlogData, key: number) => (
-                <Link
-                  href={`/blog-post/${item.attributes.slug}`}
-                  className='slidingLink border-0 border-b-[1px] border-dashed border-b-[#333333] bg-gradient-to-r from-white to-white py-8 md:border-b-0 md:py-6'
-                  key={`blog-${key}`}
-                >
-                  <div className='w-full'>
-                    <BlogCard>
-                      <div className='flex flex-row items-center justify-between'>
-                        <BlogCard.Date>
-                          Tuesday, October 25th 2022
-                        </BlogCard.Date>
-                        <Badge variant={item.attributes.status.value}>
-                          {item.attributes.status.title}
-                        </Badge>
-                      </div>
-                      <BlogCard.Title>{item.attributes.title}</BlogCard.Title>
-                      <BlogCard.Body>
-                        {item.attributes.description}
-                      </BlogCard.Body>
-                    </BlogCard>
-                  </div>
-                </Link>
-              ))}
-            </div>
+            {blogs ? (
+              <div className='mt-20 flex flex-col md:grid md:grid-cols-2 md:gap-10'>
+                {blogs.map((item: BlogData, key: number) => (
+                  <Link
+                    href={`/blog-post/${item.attributes.slug}`}
+                    className='slidingLink border-0 border-b-[1px] border-dashed border-b-[#333333] bg-gradient-to-r from-white to-white py-8 md:border-b-0 md:py-6'
+                    key={`blog-${key}`}
+                  >
+                    <div className='w-full'>
+                      <BlogCard>
+                        <div className='flex flex-row items-center justify-between'>
+                          <BlogCard.Date>
+                            Tuesday, October 25th 2022
+                          </BlogCard.Date>
+                          <Badge variant={item.attributes.status.value}>
+                            {item.attributes.status.title}
+                          </Badge>
+                        </div>
+                        <BlogCard.Title>{item.attributes.title}</BlogCard.Title>
+                        <BlogCard.Body>
+                          {item.attributes.description}
+                        </BlogCard.Body>
+                      </BlogCard>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            ) : null}
           </div>
         </section>
       </main>
