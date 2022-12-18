ab0666c62e3ccb020c7275ff4ed8f77390086402
Ivan Ilichev <swggnuko.dev@gmail.com>
2022-12-18 17:11:47 +0300

    feat: :sparkles: added blog card

---
+++ src/components/BlogCard/BlogCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import clsxm from '@/lib/clsxm';
+
+interface BlogCardModulesProps {
+  children: React.ReactNode;
+}
+
+interface BlogCardProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+function BlogDate({ children }: BlogCardModulesProps) {
+  return (
+    <span className='text-base font-light text-secondary'>{children}</span>
+  );
+}
+
+function BlogTitle({ children }: BlogCardModulesProps) {
+  return (
+    <span className='text-4xl font-black leading-none text-white md:text-6xl'>
+      {children}
+    </span>
+  );
+}
+
+function BlogBody({ children }: BlogCardModulesProps) {
+  return <span className='leading-5 text-white'>{children}</span>;
+}
+
+function BlogCard({ children, className }: BlogCardProps) {
+  return (
+    <div className={clsxm('relative', className)}>
+      <div className='flex w-full flex-col gap-6'>{children}</div>
+    </div>
+  );
+}
+
+BlogCard.Date = BlogDate;
+BlogCard.Title = BlogTitle;
+BlogCard.Body = BlogBody;
+
+export default BlogCard;
