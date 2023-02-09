0f37792c895217952820bd54db0d0f3062760fc9
Ivan Ilichev <swggnuko.dev@gmail.com>
2023-02-09 19:07:02 +0300

    feat: :sparkles: added mobile navigation

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,14 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import Link from 'next/link';
 import * as React from 'react';
+
+import clsxm from '@/lib/clsxm';
+import useOnClickOutside from '@/hooks/useOnClickOutside';
 
 import CustomLink from '@/components/CustomLink';
 import Logo from '@/components/Logo';
+
+import Burger from '~/svg/Burger.svg';
 
 const links = [
   { path: '/projects', name: 'Projects' },
@@ -10,20 +17,64 @@ const links = [
 ];
 
 export default function Header() {
+  const mobileNavRef = React.useRef<HTMLDivElement>(null);
+  const [showNav, setShowNav] = React.useState(false);
+  useOnClickOutside(mobileNavRef, () => setShowNav(false));
+
   return (
-    <header className='absolute top-0 z-50 w-full py-2'>
-      <div className='layout'>
-        <div className='flex items-center gap-14'>
-          <Logo />
-          <div className='flex gap-5'>
-            {links.map(({ path, name }) => (
-              <CustomLink key={path} href={path}>
-                {name}
-              </CustomLink>
-            ))}
-            <span></span>
+    <header className='fixed top-0 z-30 w-full' ref={mobileNavRef}>
+      <div className='relative h-full w-full'>
+        <div className='absolute z-40 w-full bg-dark  pt-2'>
+          <div className='layout pb-2'>
+            <div className='flex items-center justify-between gap-14 md:justify-start'>
+              <Logo />
+              <div
+                onClick={() => setShowNav((prev) => !prev)}
+                className='flex items-center text-3xl md:hidden'
+              >
+                <Burger />
+              </div>
+              <div className='hidden gap-5 md:flex'>
+                {links.map(({ path, name }) => (
+                  <CustomLink key={path} href={path}>
+                    {name}
+                  </CustomLink>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
+        <AnimatePresence>
+          {showNav ? (
+            <motion.div
+              initial={{ y: -140, opacity: 1 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -140, opacity: 1 }}
+              transition={{
+                type: 'spring',
+                stiffness: 100,
+                mass: 0.5,
+              }}
+              className='bg-dark'
+            >
+              <div className='layout pb-2 pt-12'>
+                {links.map(({ path, name }, key) => (
+                  <Link key={path} href={path}>
+                    <div
+                      className={clsxm(
+                        'py-3',
+                        key != links.length - 1 &&
+                          'border-0 border-b border-solid border-b-[#333333] '
+                      )}
+                    >
+                      <span className='font-display text-lg'>{name}</span>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </motion.div>
+          ) : null}
+        </AnimatePresence>
       </div>
     </header>
   );
