aa776e2ea10b459d1f9d357b3b2cf76887cbb5a3
Ivan Ilichev <swggnuko.dev@gmail.com>
2023-06-18 15:51:04 +0300

    feat: :sparkles: update logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,15 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
+import LogoImage from '~/images/logo.png';
+
 export default function Logo() {
   return (
-    <Link className='decoration' href='/'>
-      <span className='font-display text-3xl font-semibold text-white md:text-5xl'>
-        steezy
-      </span>
+    <Link className='decoration block w-36 md:w-44' href='/'>
+      <div className='relative h-10 w-full'>
+        <Image src={LogoImage} className='object-contain' alt='Image' fill />
+      </div>
     </Link>
   );
 }
