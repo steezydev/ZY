d0c984f2bb218553dd0a8aa387f5f4904a82a3e8
steezy <thesteezydev@gmail.com>
2023-06-24 12:33:07 +0300

    feat: :sparkles: add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,21 +3,18 @@ import * as React from 'react';
 import Layout from '@/components/Layout/Layout';
 import Seo from '@/components/Seo';
 
-import Sadface from '~/svg/Sadface.svg';
-
 export default function NotFoundPage() {
   return (
     <Layout>
       <Seo templateTitle='Not Found' />
 
       <main>
-        <section>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-            <Sadface className='text-7xl' />
-            <h1 className='mt-8 font-accent text-4xl text-white md:text-6xl'>
-              Page Not Found
-            </h1>
-          </div>
+        <section className='layout flex flex-col items-center justify-center text-center text-black'>
+          <h1 className='mt-8 font-accent text-4xl text-white md:text-6xl'>
+            404
+            <br />
+            Page Not Found
+          </h1>
         </section>
       </main>
     </Layout>
